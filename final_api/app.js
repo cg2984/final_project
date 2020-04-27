d1d121eba320718130b1352105f2b349848568f8
Clara <cg2984@nyu.edu>
2020-04-26 22:19:10 -0400

    adding ports and whatnot

--- final_api/app.js
+++ final_api/app.js
@@ -18,3 +18,11 @@ const firebaseConfig = {
 
 //initializing firebase for the app
 firebase.initializeApp(firebaseConfig);
+
+//we need to change the port to be a variable so that heroku can set it for us
+//if there is a port val then do that or (if that isnt there) use port 4000
+let port = process.env.PORT || 4000;
+
+//function is express object 
+//the two arguements are the port you have and a console log
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
