6b549f26cc350711c00622ea2a7abb42007db9de
Clara <cg2984@nyu.edu>
2020-05-16 18:07:23 -0400

    finishing up styling

--- final_webapp/final/src/components/footer.js
+++ final_webapp/final/src/components/footer.js
@@ -6,6 +6,7 @@ function Footer({CreatePostFunc, loggedIn}){
 	return (
 		<div>
 			<footer>
+				{loggedIn && <button className = "buttonOpen"><a href = "/home">Home</a></button>}
 				{loggedIn && <button className = "buttonOpen"><a href = "/createPost">Create Post</a></button>}
 			</footer>
 		</div>
